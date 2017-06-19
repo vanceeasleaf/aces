190b9587a354fc4e358672c63a23d0e64e002de7
Yang Zhou @cluster <404422239@qq.com>
2017-06-19 13:43:07 +0800

    edit setup

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: YangZhou
+# @Date:   2017-06-19 13:38:47
+# @Last Modified by:   YangZhou
+# @Last Modified time: 2017-06-19 13:38:52
 from setuptools import setup, find_packages
-import sys,os
+import sys
+import os
+version = "0.16"
 if sys.argv[-1] == 'publish':
     os.system("python setup.py sdist upload")
     os.system("python setup.py bdist_wheel upload")
@@ -7,18 +14,17 @@ if sys.argv[-1] == 'publish':
     print("  git tag -a %s -m 'version %s'" % (version, version))
     print("  git push --tags")
     sys.exit()
-if sys.argv[-1]=='zip':
+if sys.argv[-1] == 'zip':
     os.system("python setup.py sdist --formats=zip")
     sys.exit()
 setup(
-      name="aces",
-      version="0.16",
-      description="A python framework for computational physics numerical experiments.",
-      author="Yang Zhou",
-      author_email="404422239@qq.com",
-      url="https://github.com/vanceeasleaf/aces",
-      license="GPL2.0",
-      packages= find_packages(),
-      scripts=["scripts/ae"],
-	long_description=open('README.md').read()
-      )
+    name="aces",
+    version=version,
+    description="A python framework for computational physics numerical experiments.",
+    author="Yang Zhou",
+    author_email="404422239@qq.com",
+    url="https://github.com/vanceeasleaf/aces",
+    license="GPL2.0",
+    packages=find_packages(),
+    scripts=["scripts/ae"],
+    long_description=open('README.md').read())
